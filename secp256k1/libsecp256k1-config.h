81e35d313cd4add8a5210a42a28619ffc8458d5d
learnforpractice <learnforpractice@gmail.com>
2018-11-18 16:56:01 +0800

    Fix build on Windows

--- secp256k1/libsecp256k1-config.h
+++ secp256k1/libsecp256k1-config.h
@@ -20,8 +20,14 @@
 #define USE_SCALAR_INV_NUM 1
 
 //use impls best for 64-bit
-#define USE_FIELD_5X52 1
-#define USE_SCALAR_4X64 1
+
+#if defined (_MSC_VER)
+  #define USE_FIELD_10X26 1
+  #define USE_SCALAR_8X32 1
+#else
+  #define USE_FIELD_5X52 1
+  #define USE_SCALAR_4X64 1
+#endif
 
 //enable asm
 #ifdef __x86_64__
