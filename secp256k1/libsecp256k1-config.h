92d479c3f7573a7aa0de411764b64b53dcaaa0ca
learnforpractice <learnforpractice@gmail.com>
2018-11-18 16:46:29 +0800

    Fix build on Windows

--- secp256k1/libsecp256k1-config.h
+++ secp256k1/libsecp256k1-config.h
@@ -1,9 +1,17 @@
 #pragma once
 
 //optimizations that any compiler we target have
-#define HAVE_BUILTIN_CLZLL 1
-#define HAVE_BUILTIN_EXPECT 1
-#define HAVE___INT128 1
+
+#if defined (_MSC_VER)
+#else
+  #define HAVE_BUILTIN_CLZLL 1
+  #define HAVE_BUILTIN_EXPECT 1
+#endif
+
+#if defined (_MSC_VER)
+#else
+    #define HAVE___INT128 1
+#endif
 
 //use GMP for bignum
 #define HAVE_LIBGMP 1
@@ -12,8 +20,14 @@
 #define USE_SCALAR_INV_NUM 1
 
 //use impls best for 64-bit
-#define USE_FIELD_5X52 1
-#define USE_SCALAR_4X64 1
+
+#if defined (_MSC_VER)
+  #define USE_FIELD_10X26 1
+  #define USE_SCALAR_8X32 1
+#else
+  #define USE_FIELD_5X52 1
+  #define USE_SCALAR_4X64 1
+#endif
 
 //enable asm
 #ifdef __x86_64__
